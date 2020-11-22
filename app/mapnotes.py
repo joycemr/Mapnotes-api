3a5cdfdd7cb4ba861ae81be2f7a95dbbd6072867
Michael <michael.joyce@illiniarts.com>
2020-11-22 08:03:35 -0600

    pulled the notes dictionary out of the controller

--- app/mapnotes.py
+++ app/mapnotes.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse, fields, marshal_with
+from flask_restful import Resource, Api, abort, reqparse, fields, marshal_with
 
 app = Flask(__name__)
 api = Api(app)
@@ -33,9 +33,11 @@ class NotesRepository:
         self.notes_dict[id] = note
         return self.notes_dict[id]
 
-class MapnotesController(Resource):
+    def delete_note(self, id):
+        self.notes_dict.pop(id)
 
-    notesRepo = NotesRepository()
+notesRepo = NotesRepository()
+class MapnotesController(Resource):
 
     parser = reqparse.RequestParser()
     parser.add_argument('title', type=str)
@@ -44,18 +46,30 @@ class MapnotesController(Resource):
     @marshal_with(resource_fields)
     def get(self):
         retList = []
-        for note in self.notesRepo.notes_dict.items():
+        for note in notesRepo.notes_dict.items():
             retList.append(note[1])
         return retList
 
     @marshal_with(resource_fields)
     def post(self):
         args = self.parser.parse_args()
-        newNote = self.notesRepo.save_note(args['title'], args['body'])
+        newNote = notesRepo.save_note(args['title'], args['body'])
         return newNote
+
+# class MapnoteController:
+
+#     def delete(self, note_id):
+#         self.abort_if_todo_doesnt_exist(note_id)
+#         notesRepo.delete_note(note_id)
+#         return '', 204
+
+#     def abort_if_todo_doesnt_exist(self, note_id):
+#         if note_id not in notesRepo.notes_dict:
+#             abort(404, message="Mapnote {} doesn't exist".format(note_id))
 
 
 api.add_resource(MapnotesController, '/notes')
+# api.add_resource(MapnoteController, '/notes/<note_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
