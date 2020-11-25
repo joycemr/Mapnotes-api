a43d96650f5fc6406701798ec611f71c9f681b70
Michael <michael.joyce@illiniarts.com>
2020-11-25 14:12:36 -0600

    Prevent __pycache__

--- app/mapnotes.py
+++ app/mapnotes.py
@@ -1,3 +1,7 @@
+# This has to be at the top to prevent the cache directories
+import sys
+sys.dont_write_bytecode = True
+
 from flask import Flask, request
 from flask_restful import Resource, Api, abort, reqparse, fields, marshal_with
 from repositories.NotesRepository import notesRepo
