3439ab658b011024f829e153bb28220961c60930
Michael <michael.joyce@illiniarts.com>
2020-11-28 08:59:20 -0600

    Added the postGIS database
    1. added the docker-compose.yml file to build the container
    2. added the mapNotes.sql file to build the database
    3. all the env variables are hard coded, might change this in the future

---
+++ db/sql/mapNotes.sql
@@ -0,0 +1,33 @@
+CREATE TABLE public.notes
+(
+    id integer NOT NULL,
+    body text,
+    title varchar(255),
+    CONSTRAINT notes_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE public.note_features
+(
+    id integer NOT NULL,
+    geometry geometry,
+    notes_id integer NOT NULL,
+    CONSTRAINT note_features_pkey PRIMARY KEY (id),
+    CONSTRAINT note_fk FOREIGN KEY (notes_id)
+        REFERENCES public.notes (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+CREATE SEQUENCE public.note_seq
+    INCREMENT 50
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+CREATE SEQUENCE public.note_features_surrogate_seq
+    INCREMENT 50
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
