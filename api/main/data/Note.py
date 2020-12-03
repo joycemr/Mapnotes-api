b38c018a9249d303f16190c4c7c2eb8cd37069f6
Michael <michael.joyce@illiniarts.com>
2020-12-03 14:33:04 -0600

    refactored Note to not need up-front id
    Instead get it from the DB sequence

--- api/main/data/Note.py
+++ api/main/data/Note.py
@@ -1,3 +1,4 @@
+from typing import Sequence
 from flask_restful import fields
 from api import db
 
@@ -5,12 +6,11 @@ class Note(db.Model):
 
     __tablename__ = 'notes'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, db.Sequence('note_seq'), primary_key=True)
     title = db.Column(db.String(255))
     body = db.Column(db.Text)
 
-    def __init__(self, id, title, body):
-        self.id = id
+    def __init__(self, title, body):
         self.title = title
         self.body = body
 
