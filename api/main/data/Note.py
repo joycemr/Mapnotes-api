69defbede35c6e2fa26f90378c87b7a967c617cf
Michael <michael.joyce@illiniarts.com>
2020-12-03 13:47:38 -0600

    Making the Notes class work with the DB

--- api/main/data/Note.py
+++ api/main/data/Note.py
@@ -1,6 +1,13 @@
 from flask_restful import fields
+from api import db
 
-class Note:
+class Note(db.Model):
+
+    __tablename__ = 'notes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    body = db.Column(db.Text)
 
     def __init__(self, id, title, body):
         self.id = id
