de0b7c5d1ac7a1b59087e148d71dde0cc08b5e32
Michael <michael.joyce@illiniarts.com>
2020-12-05 10:17:15 -0600

    added env variables to __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,19 @@
+from os import getenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api, abort, reqparse, fields, marshal_with
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mapnotes_api_user:password@localhost/mapnotes_api'
+db_host = getenv('DB_HOST')
+db_user = getenv('DB_USER')
+db_password = getenv('DB_PASSWORD')
+db_name = getenv('DB_NAME')
+db_port = getenv('DB_PORT')
+
+db_uri = 'postgresql://' + db_user + ':' + db_password + '@' + db_host + '/' + db_name
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 api = Api(app)
-
