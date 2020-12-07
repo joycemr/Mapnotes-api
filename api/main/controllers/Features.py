b73a746a1f9dd9c67aa6ccb2d8ea1db1bbe9fe4f
Michael <michael.joyce@illiniarts.com>
2020-12-07 16:45:10 -0600

    stubbed out the features endpoint

---
+++ api/main/controllers/Features.py
@@ -0,0 +1,10 @@
+from flask_restful import Resource
+
+class Features(Resource):
+
+	def get(self, note_id):
+		return '{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[-7485586.996581525,-226235.48735737102],[-6305296.5676016435,-2244564.1766917016],[-8932993.786875354,-1147342.2853650213],[-8648270.63382734,78293.21631647227],[-7485586.996581525,-226235.48735737102]]]},"properties":null}]}'
+
+	def put(self, note_id):
+		pass
+
--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from api import app, api
 
 from api.main.controllers.Notes import NotesList
 from api.main.controllers.Notes import Notes
+from api.main.controllers.Features import Features
 api.add_resource(NotesList, '/notes')
 api.add_resource(Notes, '/notes/<int:note_id>')
+api.add_resource(Features, '/notes/<int:note_id>/features')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
