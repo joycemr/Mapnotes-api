69958b78e33515aeab371836f62dcfdfd2683fae
Michael <michael.joyce@illiniarts.com>
2020-12-08 17:11:35 -0600

    POST for features is:
    - reading the geojson
    - saving it to a python geojson FeatureCollection var
    - looping through the collection
    - returning the geojson from the FeatureCollection

--- api/main/controllers/Features.py
+++ api/main/controllers/Features.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource, marshal_with, reqparse
+from flask_restful import Resource
 from flask import request, jsonify
 from api.main.data.Note import resource_fields
 from geojson import FeatureCollection
@@ -6,16 +6,12 @@ from geojson import FeatureCollection
 
 class Features(Resource):
 
-    # Incoming argument parser
-    parser = reqparse.RequestParser()
-    parser.add_argument('type', type=FeatureCollection)
-
     def get(self, note_id):
         return '{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[-7485586.996581525,-226235.48735737102],[-6305296.5676016435,-2244564.1766917016],[-8932993.786875354,-1147342.2853650213],[-8648270.63382734,78293.21631647227],[-7485586.996581525,-226235.48735737102]]]},"properties":null}]}', 200
 
-    # @marshal_with(resource_fields)
     def put(self, note_id):
         fc = FeatureCollection(request.json)
-        print(type(fc))
+        for f in fc.features:
+            print(f)
         return jsonify(fc)
 
