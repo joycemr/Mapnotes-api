f761acb9a909b6da6f127c10aa6b72b9710a4a6b
Michael <michael.joyce@illiniarts.com>
2020-12-09 08:00:07 -0600

    Add docstrings to NotesRepo

--- api/main/repositories/NotesRepository.py
+++ api/main/repositories/NotesRepository.py
@@ -4,21 +4,51 @@ from api.main.data.Note import Note
 class NotesRepository:
 
     def save_note(self, title, body):
+        """ save a note to the database
+
+        Args:
+            title (String): note.title
+            body (Text): note.text
+
+        Returns:
+            Note: Note
+        """
         note = Note(title, body)
         db.session.add(note)
         db.session.commit()
         return note
 
     def get_note(self, note_id):
+        """ return a Note
+
+        Args:
+            note_id (Integer): Note.id
+
+        Returns:
+            Note: map note
+        """
         note = Note.query.get(note_id)
         if not note:
             return False
         return note
 
     def get_notes(self):
+        """ get a list of all notes
+
+        Returns:
+            [Note]: list of all notes
+        """
         return Note.query.all()
 
     def delete_note(self, note_id):
+        """ delete one note
+
+        Args:
+            note_id (Integer): note.id
+
+        Returns:
+            boolean: True if note found, False if not
+        """
         note = Note.query.get(note_id)
         if not note:
             return False
