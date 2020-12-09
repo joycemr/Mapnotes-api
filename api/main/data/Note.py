035bbf5c173eeafa2a8c7cbd3797f40c842d9742
Michael <michael.joyce@illiniarts.com>
2020-12-09 15:49:38 -0600

    Added parent-child relationship in ORM

--- api/main/data/Note.py
+++ api/main/data/Note.py
@@ -1,4 +1,5 @@
 from typing import Sequence
+from sqlalchemy.orm import backref, relationship
 from flask_restful import fields
 from api import db
 
@@ -9,19 +10,14 @@ class Note(db.Model):
     id = db.Column(db.Integer, db.Sequence('note_seq'), primary_key=True)
     title = db.Column(db.String(255))
     body = db.Column(db.Text)
-    features = []
+    noteFeatures = relationship("NoteFeature", back_populates="note")
 
     def __init__(self, title, body, **kwargs):
         self.title = title
         self.body = body
 
-    def add_feature(self, noteFeature):
-        """ add a feature to this note
-
-        Args:
-            noteFeature (geojson.Feature): a geographic feature
-        """
-        self.features.append(noteFeature)
+    def __str__(self):
+        return 'Note_id: {} Title: {} Body: {} Features: {}'.format(self.id, self.title, self.body, self.noteFeatures)
 
 resource_fields = {
     'id': fields.Integer,
--- api/main/data/NoteFeature.py
+++ api/main/data/NoteFeature.py
@@ -1,6 +1,8 @@
 from typing import Sequence
 from flask_restful import fields
 from api import db
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 from geoalchemy2 import Geometry
 
 
@@ -9,10 +11,14 @@ class NoteFeature(db.Model):
 	__tablename__ = 'note_features'
 
 	id = db.Column(db.Integer, db.Sequence('note_features_surrogate_seq'), primary_key=True)
-	geometry = (Geometry)
-	notes_id = db.Column(db.Integer)
+	geometry = db.Column(Geometry(srid=4326))
+	notes_id = db.Column(db.Integer, ForeignKey('notes.id'))
+
+	note = relationship("Note", back_populates="noteFeatures")
 
 	def __init__(self, geometry, notes_id, **kwargs):
 		self.geometry = geometry
 		self.notes_id = notes_id
 
+	def __str__(self):
+		return 'Note_id: {} Feature_id: {} Geometry'.format(self.notes_id, self.id, self.geometry)
