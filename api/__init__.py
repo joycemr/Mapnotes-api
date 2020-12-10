f062ba279d1557d06d43a776093c3b9c40c8d2fb
Michael <michael.joyce@illiniarts.com>
2020-12-10 15:57:56 -0600

    cleaned up __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,7 @@
 from os import getenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Resource, Api, abort, reqparse, fields, marshal_with
+from flask_restful import Api
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -17,7 +17,7 @@ db_uri = 'postgresql://' + db_user + ':' + db_password + '@' + db_host + '/' + d
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_ECHO'] = False
 db = SQLAlchemy(app)
 api = Api(app)
 
