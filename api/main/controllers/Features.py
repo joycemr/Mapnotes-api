6f530a17c8e1fccfc1f535c0b063923ef4c483ea
Michael <michael.joyce@illiniarts.com>
2020-12-10 15:33:37 -0600

    refactored save features into Note.py

--- api/main/controllers/Features.py
+++ api/main/controllers/Features.py
@@ -1,8 +1,6 @@
 from flask import request
 from flask_restful import Resource, abort
 from geojson import FeatureCollection
-from api.main.data.NoteFeature import NoteFeature
-from api.main.data.Note import Note
 from api.main.repositories.NotesRepository import notesRepo
 
 
@@ -18,11 +16,6 @@ class Features(Resource):
         note = notesRepo.get_note(note_id)
         if not note:
             abort(404, message="Mapnote id={} doesn't exist".format(note_id))
-        note.noteFeatures.clear()
-        featureCollection = FeatureCollection(request.json)
-        for feature in featureCollection.features:
-            noteFeature = NoteFeature(feature, note_id)
-            note.noteFeatures.append(noteFeature)
+        note.replace_featureCollection(FeatureCollection(request.json))
         notesRepo.save_note(note)
         return 'FeatureCollection added', 201
-
--- api/main/data/Note.py
+++ api/main/data/Note.py
@@ -1,6 +1,8 @@
 from flask_restful import fields
 from sqlalchemy.orm import relationship
 from api import db
+from api.main.data.NoteFeature import NoteFeature
+
 
 class Note(db.Model):
 
@@ -18,7 +20,23 @@ class Note(db.Model):
     def __str__(self):
         return 'Note_id: {} Title: {} Body: {} Features: {}'.format(self.id, self.title, self.body, self.get_featureCollection())
 
+    def replace_featureCollection(self, featureCollection):
+        """ add (or replace) the featureCollection in this object
+            each feature in the collection is written to a new NoteFeature
+            and added to this notes noteFeatures list
+        Args:
+            featureCollection (geojson.feature.FeatureCollection): a geoJson list of geometric features
+        """
+        self.noteFeatures.clear()
+        for feature in featureCollection.features:
+            noteFeature = NoteFeature(feature, self.id)
+            self.noteFeatures.append(noteFeature)
+
     def get_featureCollection(self):
+        """ return a geoJson FeatureCollection
+        Returns:
+            dict: Python dictionary that represnets a geoJson FeatureCollection
+        """
         if len(self.noteFeatures) == 0:
             return {}
         collection = {"type": "FeatureCollection", "features": []}
