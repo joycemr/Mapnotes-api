4e64a2849d80655e46b8128bb9711c4ecba4e66c
Michael <michael.joyce@illiniarts.com>
2020-12-16 09:41:52 -0600

    simple CI container test

---
+++ test_container.sh
@@ -0,0 +1,13 @@
+docker-compose up -d
+
+sleep 6s
+
+docker ps
+
+sleep 1s
+
+newman run api/test/postman_collection.json -e api/test/postman_local_environment.json
+
+sleep 1s
+
+docker-compose down
