a069be413104b2fc900c8ad60ddd58eab42e506b
Michael Joyce <michael.joyce@illiniarts.com>
2021-01-06 09:41:36 -0600

    container test changes
    1. added a new docker-compose.yml to run a db container for local dev
    2. adjusted the docker-compose test stack to run on a different port and hide the database
    3. added a persistent volume for the local dev db
    4. adjusted the newman tests accordingly

--- test_container.sh
+++ test_container.sh
@@ -6,7 +6,7 @@ docker ps
 
 sleep 1s
 
-newman run api/test/postman_collection.json -e api/test/postman_local_environment.json
+newman run api/test/postman_collection.json -e api/test/postman_container_environment.json
 
 sleep 1s
 
